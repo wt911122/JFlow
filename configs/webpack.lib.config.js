90deff93a65b84a9664b88a929066ed4b8f14e7f
汪韬 <452663934@qq.com>
2022-03-11 16:16:31 +0800

    :rocket: 修改库打包方式

---
+++ configs/webpack.lib.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        filename: 'jflow.bundle.js',
+        library: {
+            name: '@joskii/jflow',
+            type: 'umd',
+        },
+    },
+    module: {
+       rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
