8701ecbddbca243d18e0932467ae7d582bef5617
汪韬 <452663934@qq.com>
2022-03-11 11:19:43 +0800

    :wrench: 修改default导出方式

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -12,8 +14,9 @@ Object.defineProperty(exports, "JFlowVuePlugin", {
     return _JFlowPlugin["default"];
   }
 });
+exports["default"] = void 0;
 
-var _flow = require("./core/flow");
+var _flow = _interopRequireWildcard(require("./core/flow"));
 
 Object.keys(_flow).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
@@ -30,4 +33,11 @@ Object.keys(_flow).forEach(function (key) {
 var _JFlowPlugin = _interopRequireDefault(require("./vue-plugin/JFlowPlugin.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+var _default = _flow["default"];
+exports["default"] = _default;
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import { default as JFlow } from './core/flow'; 
+export default JFlow;
 export * from './core/flow';
 export { default as JFlowVuePlugin } from './vue-plugin/JFlowPlugin.js';
