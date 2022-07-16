f3e19e75b5b64e90bf0b552319981a0d02ab5f24
Joskii <wt911122@163.com>
2022-07-16 12:57:51 +0800

    :wrench: 控制台增加输出信息

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+const packageJson = require('../package.json');
 import { default as JFlow } from './core/flow'; 
 export default JFlow;
 export * from './core/flow';
 export { default as JFlowVuePlugin } from './vue-plugin/JFlowPlugin.js';
+console.log(`jflow version@${packageJson.version}`)
