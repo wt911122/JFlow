39df97c815c181730470247690d0a844902ec36e
Joskii <wt911122@163.com>
2023-03-16 10:44:00 +0800

    :zap: 更新算法，加块光标位置计算

--- src/core/instance/text-group.js
+++ src/core/instance/text-group.js
@@ -631,11 +631,12 @@ class TextGroup extends Node {
         if(textmeta.width === 0) {
             return 0;
         }
-        let idx = Math.floor(offx / textmeta.width);
+        const allwidth = textmeta.width;
+        let idx = Math.floor(offx / allwidth * maxL) ;
         requestCacheCanvas((ctx) => {
             ctx.font = `${this.fontSize} ${this.fontFamily}`;
-            let g1, g2;
-            do {
+            /* let g1, g2;
+             do {
                 const c0 = content.substring(0, idx-1);
                 const c1 = content.substring(0, idx);
                 const c2 = content.substring(0, idx+1);
@@ -651,7 +652,55 @@ class TextGroup extends Node {
                 } else if(g1 <= offx && g2 >= offx) {
                     break;
                 }
-            } while(idx >= 0 && idx <= maxL);
+            } while(idx >= 0 && idx <= maxL); */
+            let g1, g2;
+            let lastidx;
+
+            let c = content.substring(0, idx);
+            let c1 = content.substring(idx-1, idx);
+            let c2 = content.substring(idx, idx+1);
+            let w = ctx.measureText(c).width;
+            let w1 = ctx.measureText(c1).width;
+            let w2 = ctx.measureText(c2).width;
+            g1 = w - w1/2;
+            g2 = w + w2/2;
+            
+            do {
+                if(g1 <= offx && g2 >= offx) {
+                    break;
+                }  
+                if(g1 > offx) {
+                    // 左侧少了
+                    const spanw = g2 - offx;
+                    lastidx = idx;
+                    if(spanw < 100) {
+                        idx -= 1;
+                    } else {
+                        idx -= Math.floor(spanw / g2 * lastidx)
+                    }
+                    c = content.substring(idx, lastidx);
+                    w -= ctx.measureText(c).width;
+                } else if(g2 < offx) {
+                    // 右侧少了
+                    const spanw = offx - g1;
+                    lastidx = idx;
+                    if(spanw < 100) {
+                        idx += 1;
+                    } else {
+                        idx += Math.floor(spanw / (allwidth - g1) * (maxL - lastidx))
+                    }
+                    c = content.substring(lastidx, idx);
+                    w += ctx.measureText(c).width;
+                }
+                
+                c1 = content.substring(idx-1, idx);
+                c2 = content.substring(idx, idx+1);
+                w1 = ctx.measureText(c1).width;
+                w2 = ctx.measureText(c2).width;
+                g1 = w - w1/2;
+                g2 = w + w2/2;
+            } while(idx >= 0 && idx <= maxL)
+            
         });
         return idx;
     }
